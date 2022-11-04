5365b54387a67801d00f18d6f0f9f50a36c05d0e
oleg-code <61598519+oleg-code@users.noreply.github.com>
2022-11-04 10:08:02 +0200

    added files

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var angle = PI / 4;
+
+var slider;
+function setup() {
+  createCanvas(400, 400);
+  slider = createSlider(0, TWO_PI, PI/4, 0.01);
+}
+function draw() {
+  background(34);
+  angle = slider.value();
+  stroke(255, 192, 203);
+  translate(200, height);
+  branch(100);
+}
+ function branch(len) {
+  line(0, 0, 0, -len);
+  translate(0, -len);
+  if (len > 4) {
+    push();
+    rotate(angle);
+    branch(len * 0.67);
+    pop();
+    push();
+    rotate(-angle);
+    branch(len * 0.67);
+    pop();
+  }
+ }
